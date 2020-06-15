4d19a2ae8c539966f8070198555ae7847452e0e4
Alan Vinicius <alan123vinicius@gmail.com>
2020-06-14 22:34:18 -0300

    Delete view.controller.js

--- Carrinho-html,css,js,nodejs/control/view.controller.js
+++
@@ -1,7 +0,0 @@
-exports.view = function (req, res) {
-    const __dirname = "./";
-   // res.sendFile (__dirname + "/home.html");
-   res.sendFile('view/home.html' , { root : __dirname});
-};
-
-exports.module = this.view;
